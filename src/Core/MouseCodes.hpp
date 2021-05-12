71f2c86f05300e26c2705fe251daad60234c9541
James <jameskr97@gmail.com>
2021-05-12 18:06:09 -0400

    Add KeyCodes and MouseCodes header files to keep separate from codebase

---
+++ src/Core/MouseCodes.hpp
@@ -0,0 +1,21 @@
+#ifndef CORE_MOUSECODES_HPP
+#define CORE_MOUSECODES_HPP
+
+namespace Core {
+    class enum : MouseCode {
+        Button0                = 0,
+        Button1                = 1,
+        Button2                = 2,
+        Button3                = 3,
+        Button4                = 4,
+        Button5                = 5,
+        Button6                = 6,
+        Button7                = 7,
+        ButtonLast             = Button7,
+        ButtonLeft             = Button0,
+        ButtonRight            = Button1,
+        ButtonMiddle           = Button2
+    };
+}
+
+#endif CORE_KEYCODES_HPP
